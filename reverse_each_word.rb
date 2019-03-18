b20a66e386f119c2f6859745cf3530a4d9d85ce7
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-18 22:36:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   sentence.reverse 
   sentence = sentence.split
-  sentence.each do |word|
-     word.reverse 
-  end 
+  #sentence.each do |word|
+   #  word.reverse 
+  #end 
 end 

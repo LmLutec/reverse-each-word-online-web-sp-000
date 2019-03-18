169b744d82c3c1e8aac46b36084f0689837d9928
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-18 22:30:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ require 'pry'
 def reverse_each_word(sentence)
   sentence = sentence.split
   sentence.each do |word|
-    word.reverse 
-    sentence = sentence.join(" ")
-    binding.pry 
+    word = word.reverse 
+    word = word.join(" ")
+    #binding.pry 
   end 
 end 
